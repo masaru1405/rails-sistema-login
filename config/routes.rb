12e94849b03d3abf7915ce4b74873c1fbe041cd2
Kaio Masaru Nakazono <kmn1405.cg@gmail.com>
2021-09-19 12:22:25 +0000

    route -> skip: [:registrationss]

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   
   devise_for :admins
-  devise_for :users
+  devise_for :users, skip: [:registrations]
 
  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
